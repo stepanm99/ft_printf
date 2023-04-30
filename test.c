8a0c69c54fc61591dc8a70cf1673e95204776785
smelicha <smelicha@student.42heilbronn.de>
2023-04-30 19:31:00 +0200

    updates

--- test.c
+++ test.c
@@ -7,13 +7,24 @@ int	ft_print(const char *fmt, ...)
 	va_list	args;
 
 	va_start (args, fmt);
-	while (fmt)
+	while (*fmt != '\0')
 	{
-		while (*fmt != '\0')
-		{
+		if (*fmt != '%')
 			write(1, fmt, 1);
-			fmt++;;
+		if (*fmt == '%')
+		{
+			fmt++;
+			if (*fmt == 'q')
+			{
+				char	*test = va_arg(args, char *);
+				while (*test != '\0')
+				{
+					write(1, test, 1);
+					test++;
+				}
+			}
 		}
+		fmt++;
 	}
 	va_end (args);
 	return (0);
@@ -21,9 +32,7 @@ int	ft_print(const char *fmt, ...)
 
 int	main(void)
 {
-	char	nr1[] = "test1\n";
-	char	nr2[] = "test2\n";
-	char	nr3[] = "12345";
-	ft_print(nr1, nr2, nr3);
+	char	test[] = "\nvariable from the argument\n\n\n";
+	ft_print("test of the string %q", test);
 	return (0);
 }
