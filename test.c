3fc33cda0eb37d556b0b78528bad6f7a0c31fa51
smelicha <smelicha@student.42heilbronn.de>
2023-04-30 18:27:10 +0200

    learning to use va macros

--- test.c
+++ test.c
@@ -5,13 +5,25 @@
 int	ft_print(const char *fmt, ...)
 {
 	va_list	args;
-	va_start(args, fmt);
 
+	va_start (args, fmt);
+	while (fmt)
+	{
+		while (*fmt != '\0')
+		{
+			write(1, fmt, 1);
+			fmt++;;
+		}
+	}
+	va_end (args);
 	return (0);
 }
 
-int	main()
+int	main(void)
 {
-	ft_print("test", of, this, thing);
-	return(0);
+	char	nr1[] = "test1\n";
+	char	nr2[] = "test2\n";
+	char	nr3[] = "12345";
+	ft_print(nr1, nr2, nr3);
+	return (0);
 }
