57900ae45f843c4dbdba7dc17c8798c00e758332
smelicha <smelicha@student.42heilbronn.de>
2023-05-16 19:49:17 +0200

    makefile update so fsoares test works and added new txt file with failed fsoares tests

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/01 18:40:25 by smelicha          #+#    #+#              #
-#    Updated: 2023/05/11 22:38:34 by smelicha         ###   ########.fr        #
+#    Updated: 2023/05/16 19:38:57 by smelicha         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ SRC = ft_printf.c ft_datainit.c ft_check_flag.c ft_print_character.c \
 	ft_print_decimal.c ft_print_flag.c ft_print_hex_low.c ft_print_hex_up.c \
 	ft_print_integer.c ft_print_percent.c ft_print_pointer.c ft_print_string.c \
 	ft_print_unsigned_int.c ft_type_resolve.c ft_char_comp.c ft_utoa.c \
-	ft_hextoa.c ft_print_pad.c Libft/*.c leakcheck.c
+	ft_hextoa.c ft_print_pad.c
 
 CC = cc
 FLAGS = -Wall -Wextra -Werror
@@ -27,6 +27,8 @@ all: $(NAME) libft
 
 $(NAME): $(OBJ)
 	@echo "Linking $@"
+	@make -C ./Libft
+	@cp ./Libft/libft.a $(NAME)
 	@ar rcs $(NAME) $(OBJ)
 	@echo "Done!"
 
--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include "ft_printf.h"
 #include <limits.h>
 
-void	check_leaks();
+//void	check_leaks();
 
 int	main(void)
 {
-	int	count_my;
-	int	count_og;
+	int		count_my;
+	int		count_og;
+	long	i;
 
 	count_my = 0;
 	count_og = 0;
-/*	printf("--------------------------\n");
+	i = 0;
+	printf("--------------------------\n");
 	count_my = ft_printf(" %u %u %u %u %u %u %u", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
 	printf("\n--------------------------\n");
 	count_og = printf(" %u %u %lu %lu %lu %u %u", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
 	printf("\n--------------------------\n");
 	printf("RETURNS:\nmy function:\t%i\nog function:\t%i\n", count_my, count_og);
-	printf("\n--------------------------\n");*/
+	printf("\n--------------------------\n");
 
 	// printf("--------------------------\n");
 	// count_my = ft_printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
@@ -43,12 +45,12 @@ int	main(void)
 	// printf("\n--------------------------\n");
 
 	printf("--------------------------\n");
-	count_my = ft_printf(" %u ", LONG_MAX);
+	count_my = ft_printf(" %u %u %u %u %u %u %u", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
 	printf("\n--------------------------\n");
-	count_og = printf(" %lu ", LONG_MAX);
+	count_og = printf(" %u %u %lu %lu %lu %u %u", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
 	printf("\n--------------------------\n");
 	printf("RETURNS:\nmy function:\t%i\nog function:\t%i\n", count_my, count_og);
 	printf("\n--------------------------\n");
-	check_leaks();
+//	check_leaks();
 	return (0);
 }
