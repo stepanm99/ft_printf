7f064ec35da9392b37f58239d46f927b2e51ee05
smelicha <smelicha@student.42heilbronn.de>
2023-05-19 23:55:51 +0200

    update

--- main.c
+++ main.c
@@ -91,9 +91,9 @@ int	main(void)
 
 //*
 	printf("--------------------------\n");
-	count_my = ft_printf(" %.2u ", 1);
+	count_my = ft_printf(" % d ", 0);
 	printf("\n--------------------------\n");
-	count_og = printf(" %.2u ", 1);
+	count_og = printf(" % d ", 0);
 	printf("\n--------------------------\n");
 	printf("RETURNS:\nmy function:\t%i\nog function:\t%i\n", count_my, count_og);
 	printf("\n--------------------------\n");//*/
